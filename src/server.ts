9295253135e7e5b35192fc140488c02067ea224a
brunomffl <brunocardoso07@outlook.com>
2025-04-02 00:04:45 -0300

    Adicionando o express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-const number: Number = 12;
+import express from "express";
+
+//teste de servidor:
+const PORT = 3333;
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
