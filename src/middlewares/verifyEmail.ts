2d58d5f460fb3480dc37b9cca1dfde15e49af0c5
brunomffl <brunocardoso07@outlook.com>
2025-05-04 21:55:29 -0300

    Criando verificação de e-mail duplicado

---
+++ src/middlewares/verifyEmail.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import { Database } from "../database/database.js";
+
+const database = new Database();
+
+export function verificarEmail(req: Request, res: Response, next: NextFunction){
+    const { email } = req.body;
+
+    if(!email){
+        return next();
+    };
+
+    const usuarios = database.select("usuarios");
+    const usuarioId = req.params.id;
+
+
+    const emailExiste = usuarios.some((usuario) => { //percorre todos os usuarios e verifica se existe algum com o que queremos usando o some.
+        if(usuarioId && usuario.id === usuarioId){
+            //verifica se estamos atualizando. Isso verifica a partir do parâmetro opcional na url, se tiver, quer dizer que estamos atualizando um usuário e pode ignorar a verificação.
+            return false;
+        }
+        //aqui verifica em caso de adicionar um novo usuário, caso exista um email igual já cadastrado retorna true
+        return usuario.email === email;
+    })
+
+    if(emailExiste){
+        return res.status(400).json({ message: "Este e-mail já está sendo utilizado por outro usuário!" })
+    }
+
+    return next();
+}
